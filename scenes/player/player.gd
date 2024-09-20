77338d93f3f7d0aac547491b015b153b55e064f8
Michael Campos <mjcampos@users.noreply.github.com>
2024-09-20 15:18:57 +0200

    Updated player.gd
    Set fixed x position to keep the X position locked

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,9 +2,16 @@ extends CharacterBody2D
 
 @export var _speed: int = 200
 
+var fixed_x_position: float
+
+func _ready():
+	fixed_x_position = position.x
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var direction: int = Input.get_axis("ui_up", "ui_down")
 
 	velocity.y += _speed * delta * direction
 	move_and_slide()
+	
+	position.x = fixed_x_position
