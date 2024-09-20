354b244678a694ee5f81e433efbb807b428f01d6
Michael Campos <mjcampos@users.noreply.github.com>
2024-09-20 16:18:36 +0200

    Setup Puck script
    Set it up to random shoot itself in a direction and increase its speed by 10% every time it comes into contact with either a player or wall

---
+++ scenes/puck/puck.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var _speed: float = 300.0
+
+var speed_increase: float = 1.1  # Increase speed by 10% per hit
+var min_speed: float = 200.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_random_direction()
+
+func set_random_direction():
+	randomize()
+	
+	var angle = randf_range(0, PI * 2)
+	angle = 1.5
+	velocity = Vector2(cos(angle), sin(angle)) * _speed
+	
+func _physics_process(delta):
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
+	
+	if collision:
+		velocity = collision.get_normal() * (velocity.length() * speed_increase)
