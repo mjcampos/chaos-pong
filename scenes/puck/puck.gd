7096ab41224db7aeb11ce265fcb0af313d0dc2ef
Michael Campos <mjcampos@users.noreply.github.com>
2024-09-20 16:42:42 +0200

    Added chaotic element
    Made it so puck will randomly change directions. Meant to add a degree of chaos to the game

--- scenes/puck/puck.gd
+++ scenes/puck/puck.gd
@@ -1,16 +1,20 @@
 extends CharacterBody2D
 
 @export var _speed: float = 300.0
+@export var min_time: float = 1.0  # Minimum time between direction changes
+@export var max_time: float = 5.0  # Maximum time between direction changes
+
+@onready var timer = $Timer
 
 var speed_increase: float = 1.1  # Increase speed by 10% per hit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 	set_random_direction()
+	start_random_timer()
 
 func set_random_direction():
-	randomize()
-	
 	var angle = randf_range(0, PI * 2)
 	
 	velocity = Vector2(cos(angle), sin(angle)) * _speed
@@ -20,3 +24,11 @@ func _physics_process(delta):
 	
 	if collision:
 		velocity = collision.get_normal() * (velocity.length() * speed_increase)
+
+func start_random_timer():
+	timer.wait_time = randf_range(min_time, max_time)
+	timer.start()
+	
+func _on_timer_timeout():
+	set_random_direction()  # Change the direction randomly
+	start_random_timer()  # Restart the timer with a new random interval
