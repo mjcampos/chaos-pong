e4dafcb899d4ce4ef71c99f9da2e2612a3f0ae53
Michael Campos <mjcampos@users.noreply.github.com>
2024-09-21 13:56:06 +0200

    Added Score labels
    Score labels are now used to display scores for participants

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -3,14 +3,23 @@ extends Node2D
 @export var playerScore: int = 0
 @export var enemyScore: int = 0
 
+@onready var player_score = $PlayerScore
+@onready var enemy_score = $EnemyScore
+
 func _on_player_goal_entered(body):
 	if body is Puck:
 		playerScore += 1
+		player_score.text = str(playerScore)
 		
-		body.reset_location()
+		reset_puck(body)
 
 func _on_enemy_goal_entered(body):
 	if body is Puck:
 		enemyScore += 1
+		enemy_score.text = str(enemyScore)
 		
-		body.reset_location()
+		reset_puck(body)
+
+func reset_puck(body):
+	await get_tree().create_timer(1.0).timeout
+	body.reset_location()
