c5379b331f8a3143fc851364ba32d39015a5eee4
Michael Campos <mjcampos@users.noreply.github.com>
2024-09-21 20:13:26 +0200

    Initial setup of Instructions page

--- scenes/mainMenu/main_menu.gd
+++ scenes/mainMenu/main_menu.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
+
+
+func _on_instructions_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/instructions/instructions.tscn")
