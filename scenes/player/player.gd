23a16f67ecc88d52f9e28ddc462d316a015289d5
Michael Campos <mjcampos@users.noreply.github.com>
2024-09-22 15:06:55 +0200

    Update player.gd
    Updated the movement so there won't be any more inertia when the player moves.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -8,11 +8,10 @@ func _ready():
 	fixed_x_position = position.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction: int = Input.get_axis("ui_up", "ui_down")
 
-	velocity.y += _speed * delta * direction
+	velocity.y = _speed * direction
+	position.x = fixed_x_position
 	
 	move_and_slide()
-	
-	position.x = fixed_x_position
